0225f2ec6854e5c9542b408c926bf3cbe982b71c
nut <nut753908@gmail.com>
2025-07-26 17:24:37 +0900

    add AxesHelper

--- main.js
+++ main.js
@@ -26,6 +26,9 @@ function init() {
 
   scene = new THREE.Scene();
 
+  const axesHelper = new THREE.AxesHelper(3);
+  scene.add(axesHelper);
+
   const geometry = new THREE.BoxGeometry(1, 1, 1);
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   cube = new THREE.Mesh(geometry, material);
