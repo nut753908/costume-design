d31e31daf2d5c549c352dda1c5cc861680ef1206
nut <nut753908@gmail.com>
2025-07-31 01:41:54 +0900

    add cylindrical HairBundleGeometry

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { ViewportGizmo } from "three-viewport-gizmo";
 import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 // import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { HairBundleGeometry } from "./geometries/HairBundleGeometry.js";
 
 let renderer, camera, controls, gizmo, scene;
 let baseMesh;
@@ -110,6 +111,96 @@ function init() {
   //     }
   //   );
   // }
+
+  {
+    const group = new THREE.Group();
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute("position", new THREE.Float32BufferAttribute([], 3));
+
+    const lineMaterial = new THREE.LineBasicMaterial({
+      color: new THREE.Color().setHex(0xffffff, THREE.LinearSRGBColorSpace),
+      transparent: true,
+      opacity: 1,
+    });
+    const meshMaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        checkShape: { value: false },
+        lightPos: { value: new THREE.Vector3(-5, 5, 5) },
+        threshold: { value: 0.5 },
+        baseColor: {
+          value: new THREE.Color().setHex(0xfcd7e9, THREE.LinearSRGBColorSpace),
+        },
+        shadeColor: {
+          value: new THREE.Color().setHex(0xf8c1de, THREE.LinearSRGBColorSpace),
+        },
+      },
+      uniformsNeedUpdate: true,
+      vertexShader: document.getElementById("vertexShader").textContent,
+      fragmentShader: document.getElementById("fragmentShader").textContent,
+      side: THREE.DoubleSide,
+    });
+
+    group.add(new THREE.LineSegments(geometry, lineMaterial));
+    group.add(new THREE.Mesh(geometry, meshMaterial));
+
+    //
+
+    function updateGroupGeometry(mesh, geometry) {
+      mesh.children[0].geometry.dispose();
+      mesh.children[1].geometry.dispose();
+
+      mesh.children[0].geometry = new THREE.WireframeGeometry(geometry);
+      mesh.children[1].geometry = geometry;
+    }
+
+    const data = {
+      radius: 0.5,
+      height: 1,
+      radialSegments: 8,
+      heightSegments: 1,
+    };
+    function generateGeometry() {
+      updateGroupGeometry(
+        group,
+        new HairBundleGeometry(
+          data.radius,
+          data.height,
+          data.radialSegments,
+          data.heightSegments
+        )
+      );
+    }
+    generateGeometry();
+
+    scene.add(group);
+
+    {
+      const folder = gui.addFolder("group");
+      const gFolder = folder.addFolder("geometry");
+      gFolder.add(data, "radius", 0, 3, 0.01).onChange(generateGeometry);
+      gFolder.add(data, "height", 0, 5, 0.01).onChange(generateGeometry);
+      gFolder.add(data, "radialSegments", 3, 64, 1).onChange(generateGeometry);
+      gFolder.add(data, "heightSegments", 1, 64, 1).onChange(generateGeometry);
+      const lmFolder = folder.addFolder("lineMaterial");
+      lmFolder.addColor(lineMaterial, "color");
+      lmFolder.add(lineMaterial, "opacity", 0, 1, 0.1);
+      const mmFolder = folder.addFolder("meshMaterial");
+      mmFolder.add(meshMaterial, "wireframe");
+      if (meshMaterial.isShaderMaterial) {
+        const uFolder = mmFolder.addFolder("uniforms");
+        const u = meshMaterial.uniforms;
+        uFolder.add(u.checkShape, "value").name("checkShape");
+        const lpFolder = uFolder.addFolder("lightPos");
+        lpFolder.add(u.lightPos.value, "x", -10, 10, 1);
+        lpFolder.add(u.lightPos.value, "y", -10, 10, 1);
+        lpFolder.add(u.lightPos.value, "z", -10, 10, 1);
+        uFolder.add(u.threshold, "value", 0, 1, 0.1).name("threshold");
+        uFolder.addColor(u.baseColor, "value").name("baseColor");
+        uFolder.addColor(u.shadeColor, "value").name("shadeColor");
+      }
+    }
+  }
 
   //
 
