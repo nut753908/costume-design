df78e251b8e0d0e3140393ead14971fc49935982
nut <nut753908@gmail.com>
2025-08-03 22:52:51 +0900

    refactor

--- src/geometry/control-point.js
+++ src/geometry/control-point.js
@@ -32,7 +32,7 @@ export function createControlPointGeometry(gui, cp = new ControlPoint()) {
   update();
 
   {
-    const p = Math.PI;
+    const pi = Math.PI;
     const folder = gui.addFolder("geometry");
     folder.add(cp.offset, "x", -1, 1).name("offset.x").onChange(update);
     folder.add(cp.offset, "y", -1, 1).name("offset.y").onChange(update);
@@ -42,15 +42,15 @@ export function createControlPointGeometry(gui, cp = new ControlPoint()) {
     folder.add(cp.upV, "y", -1, 1).name("up.y").onChange(uv);
     folder.add(cp.upV, "z", -1, 1).name("up.z").onChange(uv);
     folder.add(cp.upS, "radius", 0, 1).name("up.radius").onChange(us);
-    folder.add(cp.upS, "phi", 0, p).name("up.phi").onChange(us);
-    folder.add(cp.upS, "theta", -p, p).name("up.theta").onChange(us);
+    folder.add(cp.upS, "phi", 0, pi).name("up.phi").onChange(us);
+    folder.add(cp.upS, "theta", -pi, pi).name("up.theta").onChange(us);
     folder.addFolder("---").close(); // separator
     folder.add(cp.downV, "x", -1, 1).name("down.x").onChange(dv);
     folder.add(cp.downV, "y", -1, 1).name("down.y").onChange(dv);
     folder.add(cp.downV, "z", -1, 1).name("down.z").onChange(dv);
     folder.add(cp.downS, "radius", 0, 1).name("down.radius").onChange(ds);
-    folder.add(cp.downS, "phi", 0, p).name("down.phi").onChange(ds);
-    folder.add(cp.downS, "theta", -p, p).name("down.theta").onChange(ds);
+    folder.add(cp.downS, "phi", 0, pi).name("down.phi").onChange(ds);
+    folder.add(cp.downS, "theta", -pi, pi).name("down.theta").onChange(ds);
 
     const controllers = folder.controllers.filter(
       (c) => c._name.startsWith("up.") || c._name.startsWith("down.")
