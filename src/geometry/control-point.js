3c8d5f8a0b1f556e359a2266e1ef09d578cdc1d9
nut <nut753908@gmail.com>
2025-08-03 22:51:55 +0900

    remove unnecessary steps

--- src/geometry/control-point.js
+++ src/geometry/control-point.js
@@ -34,23 +34,23 @@ export function createControlPointGeometry(gui, cp = new ControlPoint()) {
   {
     const p = Math.PI;
     const folder = gui.addFolder("geometry");
-    folder.add(cp.offset, "x", -1, 1, 0.01).name("offset.x").onChange(update);
-    folder.add(cp.offset, "y", -1, 1, 0.01).name("offset.y").onChange(update);
-    folder.add(cp.offset, "z", -1, 1, 0.01).name("offset.z").onChange(update);
+    folder.add(cp.offset, "x", -1, 1).name("offset.x").onChange(update);
+    folder.add(cp.offset, "y", -1, 1).name("offset.y").onChange(update);
+    folder.add(cp.offset, "z", -1, 1).name("offset.z").onChange(update);
     folder.add(cp, "isSync");
-    folder.add(cp.upV, "x", -1, 1, 0.01).name("up.x").onChange(uv);
-    folder.add(cp.upV, "y", -1, 1, 0.01).name("up.y").onChange(uv);
-    folder.add(cp.upV, "z", -1, 1, 0.01).name("up.z").onChange(uv);
-    folder.add(cp.upS, "radius", 0, 1, 0.01).name("up.radius").onChange(us);
-    folder.add(cp.upS, "phi", 0, p, 0.01).name("up.phi").onChange(us);
-    folder.add(cp.upS, "theta", -p, p, 0.01).name("up.theta").onChange(us);
+    folder.add(cp.upV, "x", -1, 1).name("up.x").onChange(uv);
+    folder.add(cp.upV, "y", -1, 1).name("up.y").onChange(uv);
+    folder.add(cp.upV, "z", -1, 1).name("up.z").onChange(uv);
+    folder.add(cp.upS, "radius", 0, 1).name("up.radius").onChange(us);
+    folder.add(cp.upS, "phi", 0, p).name("up.phi").onChange(us);
+    folder.add(cp.upS, "theta", -p, p).name("up.theta").onChange(us);
     folder.addFolder("---").close(); // separator
-    folder.add(cp.downV, "x", -1, 1, 0.01).name("down.x").onChange(dv);
-    folder.add(cp.downV, "y", -1, 1, 0.01).name("down.y").onChange(dv);
-    folder.add(cp.downV, "z", -1, 1, 0.01).name("down.z").onChange(dv);
-    folder.add(cp.downS, "radius", 0, 1, 0.01).name("down.radius").onChange(ds);
-    folder.add(cp.downS, "phi", 0, p, 0.01).name("down.phi").onChange(ds);
-    folder.add(cp.downS, "theta", -p, p, 0.01).name("down.theta").onChange(ds);
+    folder.add(cp.downV, "x", -1, 1).name("down.x").onChange(dv);
+    folder.add(cp.downV, "y", -1, 1).name("down.y").onChange(dv);
+    folder.add(cp.downV, "z", -1, 1).name("down.z").onChange(dv);
+    folder.add(cp.downS, "radius", 0, 1).name("down.radius").onChange(ds);
+    folder.add(cp.downS, "phi", 0, p).name("down.phi").onChange(ds);
+    folder.add(cp.downS, "theta", -p, p).name("down.theta").onChange(ds);
 
     const controllers = folder.controllers.filter(
       (c) => c._name.startsWith("up.") || c._name.startsWith("down.")
