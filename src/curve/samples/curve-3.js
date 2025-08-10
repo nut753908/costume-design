949a7516cd2afb6f3bac74e00ba1e3cfc26560ae
nut <nut753908@gmail.com>
2025-08-10 21:17:31 +0900

    prepare the Curve{3,2} sample

---
+++ src/curve/samples/curve-3.js
@@ -0,0 +1,27 @@
+import * as THREE from "three";
+
+import { Curve3 } from "../curve-3.js";
+import { ControlPoint3 } from "../control-point-3.js";
+
+export const screwShapedCurve3 = new Curve3([
+  new ControlPoint3(
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(-1, 0, 0),
+    new THREE.Vector3(1, 0, 0)
+  ),
+  new ControlPoint3(
+    new THREE.Vector3(2, 0, 2),
+    new THREE.Vector3(2, 0, 1),
+    new THREE.Vector3(2, 0, 3)
+  ),
+  new ControlPoint3(
+    new THREE.Vector3(2, 2, 4),
+    new THREE.Vector3(2, 1, 4),
+    new THREE.Vector3(2, 3, 4)
+  ),
+  new ControlPoint3(
+    new THREE.Vector3(4, 4, 4),
+    new THREE.Vector3(3, 4, 4),
+    new THREE.Vector3(5, 4, 4)
+  ),
+]);
