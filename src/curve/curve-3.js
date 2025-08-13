d1640f8df84d96de9fbcbde79e0b86a1775361e9
nut <nut753908@gmail.com>
2025-08-13 21:38:13 +0900

    delete clone(), copy(), toJSON(), and fromJSON() for Curve{3,2}

--- src/curve/curve-3.js
+++ src/curve/curve-3.js
@@ -51,48 +51,4 @@ export class Curve3 extends Curve {
   get name() {
     return "curve3";
   }
-
-  /**
-   * Returns a new Curve3 with copied values from this instance.
-   *
-   * @returns {Curve3} A clone of this instance.
-   */
-  clone() {
-    return new this.constructor().copy(this);
-  }
-
-  /**
-   * Copies the values of the given Curve3 to this instance.
-   *
-   * @param {Curve3} source - The Curve3 to copy.
-   * @returns {Curve3} A reference to this Curve3.
-   */
-  copy(source) {
-    super.copy(source);
-
-    return this;
-  }
-
-  /**
-   * Serializes the Curve3 into JSON.
-   *
-   * @return {Object} A JSON object representing the serialized Curve3.
-   */
-  toJSON() {
-    const data = super.toJSON();
-
-    return data;
-  }
-
-  /**
-   * Deserializes the Curve3 from the given JSON.
-   *
-   * @param {Object} json - The JSON holding the serialized Curve3.
-   * @return {Curve3} A reference to this Curve3.
-   */
-  fromJSON(json) {
-    super.fromJSON(json);
-
-    return this;
-  }
 }
