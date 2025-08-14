af009f4ee4718e91b6cd3b1ff6ef09c83f862ffa
nut <nut753908@gmail.com>
2025-08-14 17:21:05 +0900

    close the THREE.{Scene,AxesHelper} folder

--- src/object-3d/axes-helper.js
+++ src/object-3d/axes-helper.js
@@ -9,7 +9,7 @@ import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 export function createAxesHelper(gui) {
   const helper = new THREE.AxesHelper(3);
   {
-    const folder = gui.addFolder("THREE.AxesHelper");
+    const folder = gui.addFolder("THREE.AxesHelper").close();
     folder.add(helper, "visible");
   }
   return helper;
