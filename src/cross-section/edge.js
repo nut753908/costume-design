e32ec9ced33e0b54cc88ae6e41268124a0c7da31
nut <nut753908@gmail.com>
2025-08-29 00:18:41 +0900

    add edge and edges

---
+++ src/cross-section/edge.js
@@ -0,0 +1,93 @@
+// TODO: test constructor(), clone(), copy(), toJSON(), fromJSON()
+
+/**
+ * An edge of geometry.
+ *
+ * ```js
+ * import { Edge } from "./src/cross-section/edge.js";
+ * const e = new Edge( 0, 1, false );
+ * ```
+ */
+export class Edge {
+  /**
+   * Constructs a new edge.
+   *
+   * @param {number} v1 - The index of the first vertex of the edge.
+   * @param {number} v2 - The index of the second vertex of the edge.
+   * @param {boolean} isComplete - Whether the edge loop calculation is complete.
+   */
+  constructor(v1 = -1, v2 = -1, isComplete = false) {
+    /**
+     * The index of the first vertex of the edge.
+     *
+     * @type {number}
+     */
+    this.v1 = v1;
+
+    /**
+     * The index of the second vertex of the edge.
+     *
+     * @type {number}
+     */
+    this.v2 = v2;
+
+    /**
+     * Whether the edge loop calculation is complete.
+     *
+     * @type {boolean}
+     */
+    this.isComplete = isComplete;
+  }
+
+  /**
+   * Returns a new edge with copied values from this instance.
+   *
+   * @return {Edge} A clone of this instance.
+   */
+  clone() {
+    return new this.constructor().copy(this);
+  }
+
+  /**
+   * Copies the values of the given edge to this instance.
+   *
+   * @param {Edge} source - The edge to copy.
+   * @returns {Edge} A reference to this edge.
+   */
+  copy(source) {
+    this.v1 = source.v1;
+    this.v2 = source.v2;
+    this.isComplete = source.isComplete;
+
+    return this;
+  }
+
+  /**
+   * Serializes the edge into JSON.
+   *
+   * @return {Object} A JSON object representing the serialized edge.
+   */
+  toJSON() {
+    const data = {};
+
+    data.v1 = this.v1;
+    data.v2 = this.v2;
+    data.isComplete = this.isComplete;
+
+    return data;
+  }
+
+  /**
+   * Deserializes the edge from the given JSON.
+   *
+   * @param {Object} json - The JSON holding the serialized edge.
+   * @return {Edge} A reference to this edge.
+   */
+  fromJSON(json) {
+    this.v1 = json.v1;
+    this.v2 = json.v2;
+    this.isComplete = json.isComplete;
+
+    return this;
+  }
+}
