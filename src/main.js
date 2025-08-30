c1b544e2f139547e4b72047e9cb339b1d1080cee
nut <nut753908@gmail.com>
2025-08-30 22:08:35 +0900

    add debug code for baseGroup

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import { screwShapedCurve3 } from "./curve/samples/curve-3.js";
 import { smallCircleCurve2 } from "./curve/samples/curve-2.js";
 import { createCurveGroup } from "./object-3d/group/curve.js";
 import { Tube } from "./curve/tube.js";
+import { createAllEdgeLoops } from "./cross-section/edge-loops.js";
 import { createAllEdgeLoopStacks } from "./cross-section/edge-loop-stacks.js";
 import { createTubeGroup, setTubeGroupGUI } from "./object-3d/group/tube.js";
 import { saveGui, saveClosed, loadClosed } from "./main/gui.js";
@@ -29,7 +30,7 @@ const redos = [];
 
 init();
 
-function init() {
+async function init() {
   renderer = createRenderer(animate);
   camera = createCamera();
   ({ gizmo } = createControlsAndGizmo(camera, renderer));
@@ -39,10 +40,35 @@ function init() {
   scene.add(createAxesHelper(gui));
   ms = createMaterials(gui);
 
-  // await createBaseGroup(ms).then((baseGroup) => {
-  //   if (!baseGroup) return;
-  //   scene.add(baseGroup);
-  // });
+  await createBaseGroup(ms).then((baseGroup) => {
+    if (!baseGroup) return;
+    scene.add(baseGroup);
+
+    // TODO: correctly treat triangle polygons like quad polygons.
+    // start the debug code
+    const geometry = baseGroup.children[1].geometry;
+    const indices = geometry.getIndex();
+    const vertices = geometry.getAttribute("position");
+    // const list = createAllEdgeLoops(indices);
+    const list = createAllEdgeLoopStacks(indices);
+    const group2 = new THREE.Group();
+    const folder = gui.addFolder("test");
+    list.map((v, i) => {
+      const _geometry = new THREE.BufferGeometry().setFromPoints(
+        v.getPoints(vertices)
+      );
+      const _group = new THREE.Group();
+      _group.add(new THREE.Points(_geometry, ms.cp.points));
+      _group.add(new THREE.Line(_geometry, ms.cp.line));
+      group2.add(_group);
+      _group.visible = false;
+      folder.add(_group, "visible").name(i);
+    });
+    scene.add(group2);
+    console.log(list);
+    console.log(list.map((v) => v.getPoints(vertices)));
+    // end the debug code
+  });
 
   // c = new ControlPoint3();
   // c = new ControlPoint2();
@@ -52,17 +78,10 @@ function init() {
   // group = createCurveGroup(c, ms);
   c = new Tube();
   group = createTubeGroup(c, ms);
+  group.children[0].visible = false; // debug code
   setTubeGroupGUI(gui, group); // Tube only.
   c.setGUI(gui);
   scene.add(group);
-
-  // const geometry = new THREE.CylinderGeometry(5, 5, 20, 32, 8, true);
-  const geometry = group.children[0].children[1].geometry;
-  const indices = geometry.getIndex();
-  if (indices) {
-    const edgeLoopStacks = createAllEdgeLoopStacks(indices);
-    console.log(edgeLoopStacks);
-  }
 
   save();
   gui.onOpenClose(save);
