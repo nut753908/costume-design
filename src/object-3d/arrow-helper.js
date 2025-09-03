c640288dd974496f831dc1212d60e0f2c8ab9327
nut <nut753908@gmail.com>
2025-09-04 08:44:58 +0900

    add show option and set visible to false

--- src/object-3d/arrow-helper.js
+++ src/object-3d/arrow-helper.js
@@ -2,18 +2,20 @@ import * as THREE from "three";
 
 import { GUI } from "lil-gui";
 import { createColor } from "../math/color";
-import { deleteFolder, closeFolder } from "../main/gui";
+import { deleteFolder } from "../main/gui";
 
 /**
  * @param {GUI} gui
  * @param {THREE.Vector3} [dir=(0,0,1)]
  * @param {THREE.Vector3} [origin=(0,0,0)]
+ * @param {boolean} [showDirOrigin=true]
  * @return {THREE.ArrowHelper}
  */
 export function createArrowHelper(
   gui,
   dir = new THREE.Vector3(0, 0, 1),
-  origin = new THREE.Vector3(0, 0, 0)
+  origin = new THREE.Vector3(0, 0, 0),
+  showDirOrigin = true
 ) {
   const obj = {
     dir: dir,
@@ -30,34 +32,36 @@ export function createArrowHelper(
   {
     deleteFolder(gui, "THREE.ArrowHelper");
     const folder = gui.addFolder("THREE.ArrowHelper");
-    closeFolder(folder);
+    helper.visible = false;
     folder.add(helper, "visible");
-    const dFolder = folder.addFolder("dir");
-    dFolder
-      .add(obj.dir, "x")
-      .step(0.01)
-      .onChange(() => helper.setDirection(obj.dir));
-    dFolder
-      .add(obj.dir, "y")
-      .step(0.01)
-      .onChange(() => helper.setDirection(obj.dir));
-    dFolder
-      .add(obj.dir, "z")
-      .step(0.01)
-      .onChange(() => helper.setDirection(obj.dir));
-    const oFolder = folder.addFolder("origin");
-    oFolder
-      .add(obj.origin, "x")
-      .step(0.01)
-      .onChange((v) => (helper.position.x = v));
-    oFolder
-      .add(obj.origin, "y")
-      .step(0.01)
-      .onChange((v) => (helper.position.y = v));
-    oFolder
-      .add(obj.origin, "z")
-      .step(0.01)
-      .onChange((v) => (helper.position.z = v));
+    if (showDirOrigin) {
+      const dFolder = folder.addFolder("dir");
+      dFolder
+        .add(obj.dir, "x")
+        .step(0.01)
+        .onChange(() => helper.setDirection(obj.dir));
+      dFolder
+        .add(obj.dir, "y")
+        .step(0.01)
+        .onChange(() => helper.setDirection(obj.dir));
+      dFolder
+        .add(obj.dir, "z")
+        .step(0.01)
+        .onChange(() => helper.setDirection(obj.dir));
+      const oFolder = folder.addFolder("origin");
+      oFolder
+        .add(obj.origin, "x")
+        .step(0.01)
+        .onChange((v) => (helper.position.x = v));
+      oFolder
+        .add(obj.origin, "y")
+        .step(0.01)
+        .onChange((v) => (helper.position.y = v));
+      oFolder
+        .add(obj.origin, "z")
+        .step(0.01)
+        .onChange((v) => (helper.position.z = v));
+    }
     folder
       .add(obj, "length")
       .step(0.01)
--- src/object-3d/plane-helper.js
+++ src/object-3d/plane-helper.js
@@ -2,26 +2,33 @@ import * as THREE from "three";
 
 import { GUI } from "lil-gui";
 import { createColor } from "../math/color";
-import { deleteFolder, closeFolder } from "../main/gui";
+import { deleteFolder } from "../main/gui";
 
 /**
  * @param {GUI} gui
  * @param {THREE.Plane} plane
+ * @param {boolean} [showPlaneParams=true]
  * @return {THREE.PlaneHelper}
  */
-export function createPlaneHelper(gui, plane = new THREE.Plane()) {
+export function createPlaneHelper(
+  gui,
+  plane = new THREE.Plane(),
+  showPlaneParams = true
+) {
   const obj = { hex: createColor(0xffff00) };
   const helper = new THREE.PlaneHelper(plane, 1, obj.hex);
   {
     deleteFolder(gui, "THREE.PlaneHelper");
     const folder = gui.addFolder("THREE.PlaneHelper");
-    closeFolder(folder);
+    helper.visible = false;
     folder.add(helper, "visible");
-    const nfolder = folder.addFolder("normal");
-    nfolder.add(helper.plane.normal, "x").step(0.01);
-    nfolder.add(helper.plane.normal, "y").step(0.01);
-    nfolder.add(helper.plane.normal, "z").step(0.01);
-    folder.add(helper.plane, "constant").step(0.01);
+    if (showPlaneParams) {
+      const nfolder = folder.addFolder("normal");
+      nfolder.add(helper.plane.normal, "x").step(0.01);
+      nfolder.add(helper.plane.normal, "y").step(0.01);
+      nfolder.add(helper.plane.normal, "z").step(0.01);
+      folder.add(helper.plane, "constant").step(0.01);
+    }
     folder.add(helper, "size").step(0.01);
     folder.addColor(obj, "hex").onChange((v) => {
       helper.material.color.set(v);
