7fc2dde844e85609f2db342fe282e1dffcb59f15
nut <nut753908@gmail.com>
2025-09-03 18:31:26 +0900

    change centerline to line path

--- src/object-3d/group/edge.js
+++ src/object-3d/group/edge.js
@@ -5,7 +5,7 @@ import { EdgeLoop } from "../../cross-section/edge-loop.js";
 import { EdgeLoopStack } from "../../cross-section/edge-loop-stack.js";
 
 /**
- * @param {Edge|EdgeLoop|EdgeLoopStack} edge - An edge / An edge loop / An edge loop stack
+ * @param {Edge|EdgeLoop|EdgeLoopStack|THREE.CurvePath} edge - An edge / An edge loop / An edge loop stack / A curve path
  * @param {THREE.BufferAttribute} positions - The results of geometry.getAttribute("position").
  * @param {{[k1:string]:{[k2:string]:THREE.Material}}} ms - The materials.
  * @return {THREE.Group}
@@ -13,11 +13,20 @@ import { EdgeLoopStack } from "../../cross-section/edge-loop-stack.js";
 export function createEdgeGroup(edge, positions, ms) {
   const group = new THREE.Group();
 
-  const geometry = new THREE.BufferGeometry().setFromPoints(
-    edge instanceof EdgeLoopStack
-      ? edge.getPoints(positions).flat()
-      : edge.getPoints(positions)
-  );
+  let points = null;
+  if (edge instanceof Edge) points = edge.getPoints(positions);
+  if (edge instanceof EdgeLoop) points = edge.getPoints(positions);
+  if (edge instanceof EdgeLoopStack) points = edge.getPoints(positions).flat();
+  if (edge instanceof THREE.CurvePath) points = edge.getPoints();
+  if (points === null) {
+    console.error(`\
+points === null
+- edge: ${JSON.stringify(edge)}
+`);
+    return group;
+  }
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
   group.add(new THREE.Line(geometry, ms.edge.line));
 
