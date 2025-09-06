5052ea74436077985b5fe47a4740e133cf3aec20
nut <nut753908@gmail.com>
2025-09-06 21:29:01 +0900

    resolve module error

--- src/geometry/base.ts
+++ src/geometry/base.ts
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 /**
  * @return {Promise<?THREE.BufferGeometry>}
--- src/main/controls.ts
+++ src/main/controls.ts
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
-import { OrbitControls } from "three/addons/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { ViewportGizmo } from "three-viewport-gizmo";
 
 /**
