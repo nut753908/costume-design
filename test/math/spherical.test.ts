61f4d66b47b8018c2367e6d659eb863c5e3d8b4a
nut <nut753908@gmail.com>
2025-09-10 04:55:53 +0900

    add spherical test

---
+++ test/math/spherical.test.ts
@@ -0,0 +1,38 @@
+import { Spherical } from "src/math/spherical";
+import { describe, expect, test } from "vitest";
+
+describe("Spherical", () => {
+  describe("constructor()", () => {
+    test("with no params", () => {
+      const s = new Spherical();
+      expect(s.radius).toBe(1);
+      expect(s.phi).toBe(0);
+      expect(s.theta).toBe(0);
+    });
+
+    test("with params", () => {
+      const s = new Spherical(2, Math.PI / 2, Math.PI);
+      expect(s.radius).toBe(2);
+      expect(s.phi).toBe(Math.PI / 2);
+      expect(s.theta).toBe(Math.PI);
+    });
+  });
+
+  test("toJSON()", () => {
+    const json1 = new Spherical(2, Math.PI / 2, Math.PI).toJSON();
+    const json2 = { radius: 2, phi: Math.PI / 2, theta: Math.PI };
+    expect(json1).toEqual(json2);
+  });
+
+  test("fromJSON()", () => {
+    const s1 = new Spherical().fromJSON({
+      radius: 2,
+      phi: Math.PI / 2,
+      theta: Math.PI,
+    });
+    const s2 = new Spherical(2, Math.PI / 2, Math.PI);
+    expect(s1.radius).toBe(s2.radius);
+    expect(s1.phi).toBe(s2.phi);
+    expect(s1.theta).toBe(s2.theta);
+  });
+});
