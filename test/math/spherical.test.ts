2b84d42a070bf2faa71400843a4579ac3a7a327d
nut <nut753908@gmail.com>
2025-09-11 11:12:33 +0900

    fix constructor() test

--- test/math/spherical.test.ts
+++ test/math/spherical.test.ts
@@ -2,20 +2,11 @@ import { Spherical } from "src/math/spherical";
 import { describe, expect, test } from "vitest";
 
 describe("Spherical", () => {
-  describe("constructor()", () => {
-    test("default params", () => {
-      const s = new Spherical();
-      expect(s.radius).toBe(1);
-      expect(s.phi).toBe(0);
-      expect(s.theta).toBe(0);
-    });
-
-    test("specified params", () => {
-      const s = new Spherical(2, Math.PI / 2, Math.PI);
-      expect(s.radius).toBe(2);
-      expect(s.phi).toBe(Math.PI / 2);
-      expect(s.theta).toBe(Math.PI);
-    });
+  test("constructor()", () => {
+    const s = new Spherical(2, Math.PI / 2, Math.PI);
+    expect(s.radius).toBe(2);
+    expect(s.phi).toBe(Math.PI / 2);
+    expect(s.theta).toBe(Math.PI);
   });
 
   test("toJSON()", () => {
