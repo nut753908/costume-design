18485c612ce73de16891887bc298d03c3216f317
nut <nut753908@gmail.com>
2025-09-13 13:00:14 +0900

    refactor

--- src/cross-section/edges.ts
+++ src/cross-section/edges.ts
@@ -54,16 +54,12 @@ export function findNextEdge(
     console.error("e2 === null");
     return null;
   }
-  const vs0 = map[`${e2.v1},${e2.v2}`];
-  if (vs0 === undefined) return null;
+  const vs = map[`${e2.v1},${e2.v2}`];
+  if (vs === undefined) return null;
   const e3_0 =
-    vs0.length >= 1 && vs0[0].length === 2
-      ? new Edge(vs0[0][0], vs0[0][1])
-      : null;
+    vs.length >= 1 && vs[0].length === 2 ? new Edge(vs[0][0], vs[0][1]) : null;
   const e3_1 =
-    vs0.length >= 2 && vs0[1].length === 2
-      ? new Edge(vs0[1][0], vs0[1][1])
-      : null;
+    vs.length >= 2 && vs[1].length === 2 ? new Edge(vs[1][0], vs[1][1]) : null;
   if (e1 === null) return e3_0;
   if (e3_0?.equals(e1)) return e3_1;
   return e3_0;
