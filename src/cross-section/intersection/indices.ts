9a644700172cb7788baf022274bec040367ce6fb
nut <nut753908@gmail.com>
2025-09-16 14:51:18 +0900

    add createIndicesMap()

--- src/cross-section/intersection/indices.ts
+++ src/cross-section/intersection/indices.ts
@@ -19,3 +19,29 @@ export function convertToTriangularPolygonIndices(
   }
   return triangularPolygonIndices;
 }
+
+/**
+ * Create the indices map.
+ *
+ * @param nPolygonIndices - The n polygon indices.
+ * @return  The indices map. The key is a string of one or two vertices.
+ */
+export function createIndicesMap(nPolygonIndices: number[][]): {
+  [k: string]: number[][];
+} {
+  const map: { [k: string]: number[][] } = {};
+  nPolygonIndices.forEach((list) => {
+    for (let i = 0, l = list.length; i < l; i++) {
+      const a = list[i];
+      const b = i + 1 < l ? list[i + 1] : list[0];
+      [`${a}`, `${a},${b}`, `${b},${a}`].forEach((k) => {
+        if (k in map) {
+          map[k].push(list);
+        } else {
+          map[k] = [list];
+        }
+      });
+    }
+  });
+  return map;
+}
