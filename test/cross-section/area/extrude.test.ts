f8b2ea1fbcd222e3b1fd27412ff90ba01bd1eea7
nut <nut753908@gmail.com>
2025-09-25 17:36:02 +0900

    rename

--- test/cross-section/area/extrude.test.ts
+++ test/cross-section/area/extrude.test.ts
@@ -90,7 +90,7 @@ positions.array.length !== normals.array.length
     });
 
     // Import from test/cross-section/area/find.test.ts.
-    test("cube example", () => {
+    test("upper half cube example", () => {
       const SQRT1_3 = Math.sqrt(1 / 3);
       const SQRT1_2 = Math.SQRT1_2;
 
@@ -220,7 +220,7 @@ describe("flipNormals()", () => {
   });
 
   // Import from test/cross-section/area/find.test.ts.
-  test("cube example", () => {
+  test("upper half cube example", () => {
     const SQRT1_3 = Math.sqrt(1 / 3);
     const SQRT1_2 = Math.SQRT1_2;
 
@@ -352,7 +352,7 @@ describe("findBoundaries()", () => {
     });
 
     // Import from test/cross-section/area/find.test.ts.
-    test("cube example", () => {
+    test("upper half cube example", () => {
       const indicesArray = [
         [5, 8, 9],
         [4, 5, 6],
